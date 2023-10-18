6779e92b3c0189073e9d244caa3125f21dbb3d91
Shukh94 <108126378+Shukh94@users.noreply.github.com>
2023-10-19 02:18:49 +0600

    updated api key

--- js/index.js
+++ js/index.js
@@ -45,10 +45,9 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "f7af430b9dd12626ceea8cea065114b4";
-  let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${apiKey}`;
-
-  axios.get(apiURL).then(displayForecast);
+  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showTemp(response) {
@@ -82,7 +81,7 @@ function showTemp(response) {
 }
 
 function search(city) {
-  let apiKey = "f7af430b9dd12626ceea8cea065114b4";
+  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiURL).then(showTemp);
 }
