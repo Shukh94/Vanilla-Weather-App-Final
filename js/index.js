66ded3524b4c7f27e23e1a45f67add26cc5d24e4
Shukh94 <108126378+Shukh94@users.noreply.github.com>
2023-10-18 23:36:33 +0600

    added forecast js

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHtml = `<div class="row">`;
@@ -42,13 +42,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHtml;
 }
 
-function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "f7af430b9dd12626ceea8cea065114b4";
-  let apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=metric`;
+// function getForecast(coordinates) {
+//   console.log(coordinates);
+//   let apiKey = "f7af430b9dd12626ceea8cea065114b4";
+//   let apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
-  axios.get(apiURL).then(displayForecast);
-}
+//   axios.get(apiURL).then(displayForecast);
+// }
 
 function showTemp(response) {
   celsiusTemperature = response.data.main.temp;
@@ -119,3 +119,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Paris");
+displayForecast();
